e973e0387969b0848476f4b92848571d6a9ed21a
xeis-mac <xeis-mac@Mac-mini-de-xeis-mac.local>
2018-07-03 11:25:03 -0500

    codigo de crud.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var app = express()
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+app.get('/', function(req, res) {
+	// render to views/index.ejs template file
+	res.render('index', {title: 'My Node.js Application'})
+})
 
-module.exports = router;
+/** 
+ * We assign app object to module.exports
+ * 
+ * module.exports exposes the app object as a module
+ * 
+ * module.exports should be used to return the object 
+ * when this file is required in another module like app.js
+ */ 
+module.exports = app;
