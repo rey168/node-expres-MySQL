b6d84cf99cebab304b6b174c4937bdec854e5fa5
xeis-mac <xeis-mac@Mac-mini-de-xeis-mac.local>
2018-07-04 18:15:25 -0500

    Se sube nueva estructura de servicios que regresar .Json y codigos de error,mensajes de error.

--- routes/users.js
+++ routes/users.js
@@ -1,230 +1,174 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
+
+// Login POST ACTION
+app.post('/loginUsuario', function(req, res, next) {
+    var email = req.body.email;
+    var password = req.body.password;
+    req.assert('email', 'Se requiere un e-mail o válido.').isEmail(); //Validate email
+    req.assert('password', 'Contraseña es requerida').notEmpty(); //Validate password
+
+    var errors = req.validationErrors();
+
+    if (!errors) { //No errors were found.  Passed Validation!
+        req.getConnection(function(error, conn) {
+            conn.query('SELECT * FROM users WHERE email = ?', [email], function(err, rows, result) {
+                if (rows.length > 0) {
+                    if (rows[0].password == password) {
+                        res.json({
+                            message: 'sesión iniciada correctamente',
+                            codigo: '0'
+                        });
+                    } else {
+                        res.json({
+                            message: 'Correo o Contraseña incorrecta.',
+                            codigo: '1'
+                        });
+                    }
+                } else {
+                    res.json({
+                        message: 'Correo o Contraseña incorrecta.',
+                        codigo: '1'
+                    });
+                }
+            })
+        })
+    } else { //Display errors to user
+        var error_msg = ''
+        errors.forEach(function(error) {
+            error_msg += error.msg + ' , '
+        })
+        res.json({
+            message: error_msg
+        });
+    }
+})
 // SHOW LIST OF USERS
-app.get('/', function(req, res, next) {
-	req.getConnection(function(error, conn) {
-		conn.query('SELECT * FROM users ORDER BY id DESC',function(err, rows, fields) {
-			//if(err) throw err
-			if (err) {
-				req.flash('error', err)
-				res.render('user/list', {
-					title: 'User List', 
-					data: ''
-				})
-			} else {
-				// render to views/user/list.ejs template file
-				res.render('user/list', {
-					title: 'User List', 
-					data: rows
-				})
-			}
-		})
-	})
+app.get('/listaUsuarios', function(req, res, next) {
+
+    req.getConnection(function(error, conn) {
+        conn.query('SELECT * FROM users ORDER BY id DESC', function(err, rows, fields) {
+            res.json({
+                codigo: '0',
+                lista: rows
+            });
+        })
+    })
 })
 
-// SHOW ADD USER FORM
-app.get('/add', function(req, res, next){	
-	// render to views/user/add.ejs
-	res.render('user/add', {
-		title: 'Add New User',
-		name: '',
-		age: '',
-		email: ''		
-	})
-})
 
 // ADD NEW USER POST ACTION
-app.post('/add', function(req, res, next){	
-	req.assert('name', 'Name is required').notEmpty()           //Validate name
-	req.assert('age', 'Age is required').notEmpty()             //Validate age
-    req.assert('email', 'A valid email is required').isEmail()  //Validate email
+app.post('/agregarUsuarios', function(req, res, next) {
+    req.assert('name', 'Nombre es requerido').notEmpty(); //Validate name
+    req.assert('age', 'Edad es requerida').notEmpty(); //Validate age
+    req.assert('email', 'Se requiere un e-mail o válido.').isEmail(); //Validate email
+		req.assert('password', 'Contraseña es requerida').notEmpty(); //Validate password
 
     var errors = req.validationErrors()
-    
-    if( !errors ) {   //No errors were found.  Passed Validation!
-		
-		/********************************************
-		 * Express-validator module
-		 
-		req.body.comment = 'a <span>comment</span>';
-		req.body.username = '   a user    ';
 
-		req.sanitize('comment').escape(); // returns 'a &lt;span&gt;comment&lt;/span&gt;'
-		req.sanitize('username').trim(); // returns 'a user'
-		********************************************/
-		var user = {
-			name: req.sanitize('name').escape().trim(),
-			age: req.sanitize('age').escape().trim(),
-			email: req.sanitize('email').escape().trim()
-		}
-		
-		req.getConnection(function(error, conn) {
-			conn.query('INSERT INTO users SET ?', user, function(err, result) {
-				//if(err) throw err
-				if (err) {
-					req.flash('error', err)
-					
-					// render to views/user/add.ejs
-					res.render('user/add', {
-						title: 'Add New User',
-						name: user.name,
-						age: user.age,
-						email: user.email					
-					})
-				} else {				
-					req.flash('success', 'Data added successfully!')
-					
-					// render to views/user/add.ejs
-					res.render('user/add', {
-						title: 'Add New User',
-						name: '',
-						age: '',
-						email: ''					
-					})
-				}
-			})
-		})
-	}
-	else {   //Display errors to user
-		var error_msg = ''
-		errors.forEach(function(error) {
-			error_msg += error.msg + '<br>'
-		})				
-		req.flash('error', error_msg)		
-		
-		/**
-		 * Using req.body.name 
-		 * because req.param('name') is deprecated
-		 */ 
-        res.render('user/add', { 
-            title: 'Add New User',
-            name: req.body.name,
-            age: req.body.age,
-            email: req.body.email
+    if (!errors) { //No errors were found.  Passed Validation!
+
+        var user = {
+            name: req.sanitize('name').escape().trim(),
+            age: req.sanitize('age').escape().trim(),
+            email: req.sanitize('email').escape().trim(),
+						password: req.sanitize('password').escape().trim()
+        }
+
+        req.getConnection(function(error, conn) {
+            conn.query('INSERT INTO users SET ?', user, function(err, result) {
+                //if(err) throw err
+                if (err) {
+                    res.json({
+                        message: 'Error al crear usuario.',
+                        codigo: '1'
+                    });
+                } else {
+                    res.json({
+                        message: 'Usuario creado con éxito.',
+                        codigo: '0'
+                    });
+                }
+            })
         })
+    } else { //Display errors to user
+        var error_msg = ''
+        errors.forEach(function(error) {
+            error_msg += error.msg + ' , '
+        })
+        res.json({
+            message: error_msg
+        });
     }
 })
 
-// SHOW EDIT USER FORM
-app.get('/edit/(:id)', function(req, res, next){
-	req.getConnection(function(error, conn) {
-		conn.query('SELECT * FROM users WHERE id = ' + req.params.id, function(err, rows, fields) {
-			if(err) throw err
-			
-			// if user not found
-			if (rows.length <= 0) {
-				req.flash('error', 'User not found with id = ' + req.params.id)
-				res.redirect('/users')
-			}
-			else { // if user found
-				// render to views/user/edit.ejs template file
-				res.render('user/edit', {
-					title: 'Edit User', 
-					//data: rows[0],
-					id: rows[0].id,
-					name: rows[0].name,
-					age: rows[0].age,
-					email: rows[0].email					
-				})
-			}			
-		})
-	})
-})
 
 // EDIT USER POST ACTION
-app.put('/edit/(:id)', function(req, res, next) {
-	req.assert('name', 'Name is required').notEmpty()           //Validate name
-	req.assert('age', 'Age is required').notEmpty()             //Validate age
-    req.assert('email', 'A valid email is required').isEmail()  //Validate email
+app.put('/editarUsuarios', function(req, res, next) {
+    req.assert('name', 'Nombre es requerido').notEmpty() //Validate name
+    req.assert('age', 'Edad es requerida').notEmpty() //Validate age
+    req.assert('email', 'Se requiere un correo o válido.').isEmail() //Validate email
 
     var errors = req.validationErrors()
-    
-    if( !errors ) {   //No errors were found.  Passed Validation!
-		
-		/********************************************
-		 * Express-validator module
-		 
-		req.body.comment = 'a <span>comment</span>';
-		req.body.username = '   a user    ';
 
-		req.sanitize('comment').escape(); // returns 'a &lt;span&gt;comment&lt;/span&gt;'
-		req.sanitize('username').trim(); // returns 'a user'
-		********************************************/
-		var user = {
-			name: req.sanitize('name').escape().trim(),
-			age: req.sanitize('age').escape().trim(),
-			email: req.sanitize('email').escape().trim()
-		}
-		
-		req.getConnection(function(error, conn) {
-			conn.query('UPDATE users SET ? WHERE id = ' + req.params.id, user, function(err, result) {
-				//if(err) throw err
-				if (err) {
-					req.flash('error', err)
-					
-					// render to views/user/add.ejs
-					res.render('user/edit', {
-						title: 'Edit User',
-						id: req.params.id,
-						name: req.body.name,
-						age: req.body.age,
-						email: req.body.email
-					})
-				} else {
-					req.flash('success', 'Data updated successfully!')
-					
-					// render to views/user/add.ejs
-					res.render('user/edit', {
-						title: 'Edit User',
-						id: req.params.id,
-						name: req.body.name,
-						age: req.body.age,
-						email: req.body.email
-					})
-				}
-			})
-		})
-	}
-	else {   //Display errors to user
-		var error_msg = ''
-		errors.forEach(function(error) {
-			error_msg += error.msg + '<br>'
-		})
-		req.flash('error', error_msg)
-		
-		/**
-		 * Using req.body.name 
-		 * because req.param('name') is deprecated
-		 */ 
-        res.render('user/edit', { 
-            title: 'Edit User',            
-			id: req.params.id, 
-			name: req.body.name,
-			age: req.body.age,
-			email: req.body.email
+    if (!errors) { //No errors were found.  Passed Validation!
+        var user = {
+            name: req.sanitize('name').escape().trim(),
+            age: req.sanitize('age').escape().trim(),
+            email: req.sanitize('email').escape().trim()
+        }
+
+        req.getConnection(function(error, conn) {
+            conn.query('UPDATE users SET ? WHERE id = ' + req.body.id, user, function(err, result) {
+                //if(err) throw err
+                if (err) {
+                    res.json({
+                        message: 'Error al modificar usuario.',
+                        codigo: '1'
+                    });
+                } else {
+                    res.json({
+                        message: 'Usuario modificado con éxito.',
+                        codigo: '0'
+                    });
+                }
+            })
         })
+    } else { //Display errors to user
+        var error_msg = ''
+        errors.forEach(function(error) {
+            error_msg += error.msg + ' , '
+        })
+        res.json({
+            message: error_msg
+        });
+
     }
 })
 
 // DELETE USER
-app.delete('/delete/(:id)', function(req, res, next) {
-	var user = { id: req.params.id }
-	
-	req.getConnection(function(error, conn) {
-		conn.query('DELETE FROM users WHERE id = ' + req.params.id, user, function(err, result) {
-			//if(err) throw err
-			if (err) {
-				req.flash('error', err)
-				// redirect to users list page
-				res.redirect('/users')
-			} else {
-				req.flash('success', 'User deleted successfully! id = ' + req.params.id)
-				// redirect to users list page
-				res.redirect('/users')
-			}
-		})
-	})
+app.delete('/eliminarUsuarios', function(req, res, next) {
+    var user = {
+        id: req.body.id
+    }
+
+    req.getConnection(function(error, conn) {
+        conn.query('DELETE FROM users WHERE id = ' + req.body.id, user, function(err, result) {
+            //if(err) throw err
+            if (err) {
+                res.json({
+                    message: "Error ala eliminar al usuario.",
+                    codigo: '1'
+                });
+            } else {
+                res.json({
+                    message: "El Usuario se elimino correctamente.",
+                    codigo: '0'
+                });
+            }
+        })
+    })
 })
 
 module.exports = app
