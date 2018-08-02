ab3139d0c068734502c6c386e0297b2890e66b60
xeis-mac <xeis-mac@Mac-mini-de-xeis-mac.local>
2018-08-01 19:59:28 -0500

    Se suben cambios a servicio agregarCliente.

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,38 @@
 var express = require('express');
+var multer  = require('multer')
 var app = express();
 
 
-// Login POST ACTION
+
+//Destino y Servicio para cargar imagenes y archivos.
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'imagenes/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + '.jpg')
+    }
+});
+
+var upload = multer({ storage: storage }).single('profileImage');
+
+  app.post('/profile', function (req, res) {
+      upload(req, res, function (err) {
+          if (err) {
+              // An error occurred when uploading
+          }
+          res.json({
+              message: 'Imagen cargada correctamente.',
+              codigo: '0'
+          });
+
+          // Everything went fine
+      })
+  });
+
+
+
+//Servicio para login.
 app.post('/loginUsuario', function(req, res, next) {
     var email = req.body.email;
     var password = req.body.password;
@@ -45,7 +75,7 @@ app.post('/loginUsuario', function(req, res, next) {
         });
     }
 })
-// SHOW LIST OF USERS
+// Lista Usuarios
 app.get('/listaUsuarios', function(req, res, next) {
 
     req.getConnection(function(error, conn) {
@@ -58,8 +88,21 @@ app.get('/listaUsuarios', function(req, res, next) {
     })
 })
 
+//Lista Clientes
+app.get('/listaClientes', function(req, res, next) {
 
-// ADD NEW USER POST ACTION
+    req.getConnection(function(error, conn) {
+        conn.query('SELECT * FROM clientes', function(err, rows, fields) {
+            res.json({
+                codigo: '0',
+                lista: rows
+            });
+        })
+    })
+})
+
+
+//Servicio pra crear nuevo usuario.
 app.post('/agregarUsuarios', function(req, res, next) {
     req.assert('name', 'Nombre es requerido').notEmpty(); //Validate name
     req.assert('age', 'Edad es requerida').notEmpty(); //Validate age
@@ -105,8 +148,65 @@ app.post('/agregarUsuarios', function(req, res, next) {
     }
 })
 
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'imagenes/')
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.fieldname + '-' + Date.now() + '.jpg')
+    }
+});
 
-// EDIT USER POST ACTION
+var upload = multer({storage: storage});
+
+//Servicio pra crear nuevo cliente.
+app.post('/agregarClientes',  upload.single('image'), (req, res, next) => {
+    req.assert('name', 'Nombre es requerido').notEmpty(); //Validate name
+    req.assert('descrip', 'Descripción es requerida').notEmpty(); //Validate age
+    req.assert('UrlWeb', 'Se requiere una Url Web.').notEmpty(); //Validate email
+
+    var errors = req.validationErrors()
+
+    if (!errors) { //No errors were found.  Passed Validation!
+
+
+        var user = {
+            name: req.sanitize('name').escape().trim(),
+            descrip: req.sanitize('descrip').escape().trim(),
+            UrlWeb: req.sanitize('UrlWeb').escape().trim(),
+            image:'imagenes/' + req.file.fieldname
+        }
+
+        req.getConnection(function(error, conn) {
+            conn.query('INSERT INTO clientes SET ?', user, function(err, result) {
+                //if(err) throw err
+                if (err) {
+                    res.json({
+                        message: 'Error al crear cliente.',
+                        codigo: '1'
+                    });
+                } else {
+                    res.json({
+                        message: 'Cliente creado con éxito.',
+                        codigo: '0'
+                    });
+                }
+            })
+        })
+    } else { //Display errors to user
+        var error_msg = ''
+        errors.forEach(function(error) {
+            error_msg += error.msg + ' , '
+        })
+        res.json({
+            message: error_msg,
+            codigo: '1'
+        });
+    }
+})
+
+
+//Servicio para editar usuarios
 app.put('/editarUsuarios', function(req, res, next) {
     req.assert('name', 'Nombre es requerido').notEmpty() //Validate name
     req.assert('age', 'Edad es requerida').notEmpty() //Validate age
@@ -150,7 +250,7 @@ app.put('/editarUsuarios', function(req, res, next) {
     }
 })
 
-// DELETE USER
+//Servicio para eliminar usuarios.
 app.delete('/eliminarUsuarios', function(req, res, next) {
     var user = {
         id: req.body.id
